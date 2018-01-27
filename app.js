028e192786ced7b2caf70a5986b0d14faf7adcf8
Zeshan Amjad <zeshan@zesh.me>
2018-01-27 17:14:44 +0000

    Allow serving of static files

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Listening on Port 3000!'));
