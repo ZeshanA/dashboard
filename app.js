affe7ffca9d32cd5f377c8749d9bb0512e781afb
Zeshan Amjad <zeshan@zesh.me>
2018-01-27 17:17:30 +0000

    Add comments in app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// Import Express
 const express = require('express');
 const app = express()
 
+// Serve Hello World as projec troot
 app.get('/', (req, res) => res.send('Hello World!'));
+// Allow serving of static files from the 'public' directory
 app.use(express.static('public'))
+// Listen on Port 3000
 app.listen(3000, () => console.log('Listening on Port 3000!'));
