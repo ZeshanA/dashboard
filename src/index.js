10b4fb4be63f7faa81154aa37a10c96797eb7dce
Zeshan Amjad <zeshan@zesh.me>
2018-01-27 16:29:34 +0000

    Empty repository

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App txt="This is the prop test" />, document.getElementById('root'));
-registerServiceWorker();
