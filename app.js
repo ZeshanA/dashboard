b3bff811daf21a0393b1639d0419159813dfc5bd
Zeshan Amjad <zeshan@zesh.me>
2018-01-27 17:36:51 +0000

    Add jade template for /

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 const express = require('express');
 const app = express()
 
+app.set("view engine", "jade");
+
 // Serve Hello World as projec troot
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.render('index'));
+
 // Allow serving of static files from the 'public' directory
 app.use(express.static('public'))
+
 // Listen on Port 3000
 app.listen(3000, () => console.log('Listening on Port 3000!'));
