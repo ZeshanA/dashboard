d0218fc13636fdb3c736c8572d6a26509ccac88e
Zeshan Amjad <zeshan@zesh.me>
2018-01-27 18:47:46 +0000

    Add basic homepage using Jade

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Import Express
 const express = require('express');
-const app = express()
+const app = express();
 
 app.set("view engine", "jade");
 
@@ -8,7 +8,7 @@ app.set("view engine", "jade");
 app.get('/', (req, res) => res.render('index'));
 
 // Allow serving of static files from the 'public' directory
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Listen on Port 3000
 app.listen(3000, () => console.log('Listening on Port 3000!'));
